89f1ea1c5914554708959cde81a40c667e9481fc
Graham West <graham.west@gmail.com>
2015-01-17 11:32:01 -0800

    humanize text and menu

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,34 +4,19 @@
 // Include app dependency on ngMaterial
 
 var DashboardApp = angular.module( 'DashboardApp', [ 'ngMaterial', 'ngRoute' ] )
-  .controller('NavController', function ($scope) {
+  .controller('NavController', function ($scope, $http) {
 
+    // load dashboard data
+    $http.get('dashboard-data.json').success(function(data) {
+      $scope.db = data;
 
-    $scope.db = {
-      'slt' : {
-        '2015-Q1': {
-          'text': 'Hello'
-        }
-      },
-      'strategy' : {
-        '2015-Q1': {
-          'text': 'bob'
-        }
-      },
-      'function' : {
-        '2015-01': {
-          'text': 'function',
-          'engineering' : {
-            'text' : 'moo'
-          }
-        }
+      // select default set to be first one in json
+      if (!$scope.selectedDashboardSetName) {
+        $scope.selectedDashboardSetName = Object.keys($scope.db.dashboards)[0];
       }
-    };
+    });
 
-
-    $scope.world = 'World!';
   });
-
 
 DashboardApp.controller('DashboardController', function ($scope, $routeParams) {
 
@@ -40,12 +25,20 @@ DashboardApp.controller('DashboardController', function ($scope, $routeParams) {
       return tree;
     }
 
-    return resolveDashboardNode( tree[path[0]], path.slice(1) );
+    return resolveDashboardNode( tree.dashboards[path[0]], path.slice(1) );
   };
 
-  var dashboard = resolveDashboardNode($scope.$parent.db, $routeParams.path.split('/'));
+  var pathTokens = $routeParams.path.split('/');
+  var dashboard = resolveDashboardNode($scope.$parent.db, pathTokens);
 
-  $scope.message = dashboard.text;
+  $scope.$parent.selectedDashboardSetName = pathTokens[0];
+  $scope.$parent.selectedDateName = pathTokens[1];
+  $scope.$parent.selectedAreaName = pathTokens[2];
+  $scope.$parent.selectedTeamName = pathTokens[3];
+
+  $scope.title = dashboard.title;
+  $scope.view = dashboard.view;
+  $scope.data = dashboard.data;
 });
 
 
@@ -62,21 +55,22 @@ DashboardApp.config(['$routeProvider', function( $routeProvider ){
 
 }]);
 
-/*
-angular.module('dashboardApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
-*/
+
+DashboardApp.filter('humanize', function() {
+  // from https://gist.github.com/juanpujol/5835379
+  return function(text) {
+    if(text) {
+      text = text.split('-');
+
+      // go through each word in the text and capitalize the first letter
+      for (var i in text) {
+        var word = text[i];
+        word = word.toLowerCase();
+        word = word.charAt(0).toUpperCase() + word.slice(1);
+        text[i] = word;
+      }
+
+      return text.join(' ');
+    }
+  };
+});
