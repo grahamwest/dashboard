21d4f56a10b776298616406f1f34b63c836c92bd
Graham West <graham.west@gmail.com>
2015-01-24 11:45:23 -0800

    somewhat decent layout

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,7 +3,7 @@
 
 // Include app dependency on ngMaterial
 
-var DashboardApp = angular.module( 'DashboardApp', [ 'ngMaterial', 'ngRoute' ] )
+var DashboardApp = angular.module( 'DashboardApp', [ 'ngMaterial', 'ngRoute', 'angularFileUpload' ] )
   .controller('NavController', function ($scope, $http) {
 
     $scope.db = {};
@@ -12,20 +12,47 @@ var DashboardApp = angular.module( 'DashboardApp', [ 'ngMaterial', 'ngRoute' ] )
       $scope.db = {};
     };
 
-    $scope.importDbJson = function(importData) {
-      $scope.db = angular.extend({}, $scope.db, importData);
+    var extendDeep = function extendDeep(dst) {
+      angular.forEach(arguments, function(obj) {
+        if (obj !== dst) {
+          angular.forEach(obj, function(value, key) {
+            if (dst[key] && dst[key].constructor && dst[key].constructor === Object) {
+              extendDeep(dst[key], value);
+            } else {
+              dst[key] = value;
+            }
+          });
+        }
+      });
+      return dst;
+    };
+
+    $scope.importFromJson = function(importData) {
+      $scope.db = extendDeep($scope.db, importData);
 
       // select default dashboard set to be first one in json if we don't already have on selected
       if (!$scope.selectedDashboardSetName) {
         $scope.selectedDashboardSetName = Object.keys($scope.db.dashboards)[0];
       }
+
     };
 
     $scope.importFromUrl = function(url) {
       // load dashboard data
       $http.get(url).success(function(data) {
-        $scope.importDbJson(data);
+        $scope.importFromJson(data);
       });
+    };
+
+    $scope.importFromFile = function(file) {
+      var reader = new FileReader();
+      reader.addEventListener('load', function() {
+        var jsonData = JSON.parse(reader.result);
+        $scope.importFromJson(jsonData);
+        $scope.$digest();
+      });
+
+      reader.readAsText(file);
     };
 
     $scope.showSidebar = true;
@@ -39,20 +66,32 @@ var DashboardApp = angular.module( 'DashboardApp', [ 'ngMaterial', 'ngRoute' ] )
       $scope.showSidebarLocked = !$scope.showSidebarLocked;
     };
 
+    $scope.dropFiles = [];
+    $scope.$watch('dropFiles', function() {
+      for (var i = 0; i < $scope.dropFiles.length; i++) {
+        var file = $scope.dropFiles[i];
+        $scope.importFromFile(file);
+      }
+    });
 
     // load default data
     $scope.importFromUrl('dashboard-data.json');
 
   });
 
-DashboardApp.controller('DashboardController', function ($scope, $routeParams) {
+DashboardApp.controller('DashboardController', function ($scope, $routeParams, $location) {
 
   var resolveDashboardNode = function( tree, path ) {
     if (path.length === 0) {
       return tree;
     }
 
-    return resolveDashboardNode( tree.dashboards[path[0]], path.slice(1) );
+    try {
+      return resolveDashboardNode( tree.dashboards[path[0]], path.slice(1) );
+    } catch (e) {
+      $location.path('/');
+      return {};
+    }
   };
 
   var pathTokens = $routeParams.path.split('/');
@@ -116,6 +155,56 @@ DashboardApp.filter('humanize', function() {
       }
 
       return text.join(' ');
+    }
+  };
+});
+
+
+DashboardApp.directive('value', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    replace: true,
+    templateUrl: 'directives/traffic-light.html',
+    scope: {
+      'label': '@',
+      'value': '=',
+      'unit': '=',
+      'lowIsGood': '=',
+      'thresholds': '='
+    },
+    link: function(scope) {
+
+      scope.color = 'gray';
+
+      var determineColorClass = function(value, thresholds, lowIsGood) {
+
+        var thresholdGroup = 0;
+        // thresholds is an array used to determine where the boundaries between red/amber/green are
+        thresholds.some(function(threshold) {
+          if (value > threshold)
+          {
+            thresholdGroup++;
+          }
+          return threshold > value;
+        });
+
+        // pick color based on thresholds
+        var colors = ['red', 'amber', 'green'];
+
+        if (lowIsGood) {
+          colors = colors.reverse();
+        }
+
+        return colors[thresholdGroup];
+      };
+
+      scope.$watchGroup(['thresholds', 'lowIsGood'], function() {
+        if (scope.thresholds) {
+          scope.color = determineColorClass(scope.value, scope.thresholds, scope.lowIsGood);
+        }
+      });
+      
     }
   };
 });
